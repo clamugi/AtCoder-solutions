6c82dbe14b3d9b8367551dc2a5e37367f574a1c2
clamugi <66073853+clamugi@users.noreply.github.com>
2020-11-18 19:19:12 +0900

    67

---
+++ ABC/67/67a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+#define repr(i,a,b) for (int i=a; i<b; i++)
+#define rep(i,n) for (int i=0; i<  n; i++)
+#define PI 3.14159265359  
+const long long INF = 1LL << 60;
+long long MOD = 1000000007;
+long long gcd(long long a, long long b) { return b ? gcd(b, a%b) : a; }
+long long lcm (int a, int b){return  (long long) a*b /gcd(a,b);}
+
+int main(){
+    int A, B;
+    cin >> A >> B;
+
+    if(A%3 == 0 || B%3 == 0 || (A+B)%3 == 0)cout << "Possible" << endl;
+    else cout <<"Impossible" << endl;
+}
