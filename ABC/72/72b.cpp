030990cc186ac89e8b2fa70264cfebed967dae2d
clamugi <66073853+clamugi@users.noreply.github.com>
2020-12-02 19:05:33 +0900

    72

---
+++ ABC/72/72b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+#define repr(i,a,b) for (int i=a; i<b; i++)
+#define rep(i,n) for (int i=0; i<  n; i++)
+#define PI 3.14159265359  
+const long long INF = 1LL << 60;
+long long MOD = 1000000007;
+long long gcd(long long a, long long b) { return b ? gcd(b, a%b) : a; }
+long long lcm (int a, int b){return  (long long) a*b /gcd(a,b);}
+
+
+int main () {
+    string S,ans;
+ 
+    cin >> S;
+ 
+    for(int i = 0; i<S.length(); i += 2){
+        ans += S[i];
+    }
+ 
+    cout << ans << endl;
+ 
+}
