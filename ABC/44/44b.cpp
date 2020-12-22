2c3ec9b6de54973c3a91d34ab496dd9b20e80dcd
clamugi <66073853+clamugi@users.noreply.github.com>
2020-12-22 17:03:37 +0900

    44

---
+++ ABC/44/44b.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+#define repr(i,a,b) for (int i=a; i<b; i++)
+#define rep(i,n) for (int i=0; i<  n; i++)
+#define PI 3.14159265359  
+const long long INF = 1LL << 60;
+long long MOD = 1000000007;
+long long gcd(long long a, long long b) { return b ? gcd(b, a%b) : a; }
+long long lcm (int a, int b){return  (long long) a*b /gcd(a,b);}
+
+int main(){
+    string w;
+    cin >> w;
+
+    vector<int>s(26,0);
+    rep(i,w.length()){
+        int a = w[i]-'a';
+        s[a]++;
+    }
+
+    bool check = true;
+    rep(i,26){
+        if(s[i] %2 == 1)check = false;
+    }
+    if(check)cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
