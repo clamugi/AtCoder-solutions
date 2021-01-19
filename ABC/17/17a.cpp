4067ac2a0aa7e63ab967ebfe745f5f31c79ae96d
clamugi <66073853+clamugi@users.noreply.github.com>
2021-01-19 23:56:40 +0900

    17

---
+++ ABC/17/17a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+#define repr(i,a,b) for (int i=a; i<b; i++)
+#define rep(i,n) for (int i=0; i<  n; i++)
+#define PI 3.14159265359  
+const long long INF = 1LL << 60;
+long long MOD = 1000000007;
+long long gcd(long long a, long long b) { return b ? gcd(b, a%b) : a; }
+long long lcm (int a, int b){return  (long long) a*b /gcd(a,b);}
+
+int main(){
+    vector<int>a(6);
+    rep(i,6)cin >> a[i];
+
+    int ans = 0;
+    rep(i,3){
+        ans += a[i*2] *a[i*2+1] /10;
+    }   
+    cout << ans << endl;
+}
