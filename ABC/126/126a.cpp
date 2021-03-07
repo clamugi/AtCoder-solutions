bfc1437bbbdd187ed53cd617e8a5b38991bf0a18
clamugi <teru_10cu@yahoo.co.jp>
2021-03-07 15:15:18 +0900

    126

---
+++ ABC/126/126a.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+#define repr(i,a,b) for (int i=a; i<b; i++)
+#define rep(i,n) for (int i=0; i<  n; i++)
+#define PI 3.14159265359  
+const long long INF = 1LL << 60;
+long long MOD = 1000000007;
+long long gcd(long long a, long long b) { return b ? gcd(b, a%b) : a; }
+long long lcm (int a, int b){return  (long long) a*b /gcd(a,b);}
+double radian(double degree){return degree * PI /180.0;}
+double degree(double radian){return radian * 180.0/PI;}
+
+vector<pair<long long, long long> > prime_factorize(long long N){
+    vector<pair< long long , long long> > res;
+    for(long long a = 2; a* a <= N; a++){
+        if(N%a != 0)continue;
+        long long ex = 0;
+        while(N%a == 0){
+            ex++;
+            N /= a;
+        }
+        res.push_back({a,ex});
+    }
+    if(N!=1)res.push_back({N,1});
+    return res;
+}
+int main(){
+    
+    int N, K;
+    string S;
+    cin >> N >> K >> S;
+ 
+    S[K-1] = S[K-1] - ('A'-'a');
+ 
+    cout << S << endl;
+ 
+
+}
---
+++ ABC/126/126b.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+#define repr(i,a,b) for (int i=a; i<b; i++)
+#define rep(i,n) for (int i=0; i<  n; i++)
+#define PI 3.14159265359  
+const long long INF = 1LL << 60;
+long long MOD = 1000000007;
+long long gcd(long long a, long long b) { return b ? gcd(b, a%b) : a; }
+long long lcm (int a, int b){return  (long long) a*b /gcd(a,b);}
+double radian(double degree){return degree * PI /180.0;}
+double degree(double radian){return radian * 180.0/PI;}
+
+vector<pair<long long, long long> > prime_factorize(long long N){
+    vector<pair< long long , long long> > res;
+    for(long long a = 2; a* a <= N; a++){
+        if(N%a != 0)continue;
+        long long ex = 0;
+        while(N%a == 0){
+            ex++;
+            N /= a;
+        }
+        res.push_back({a,ex});
+    }
+    if(N!=1)res.push_back({N,1});
+    return res;
+}
+int main(){
+    
+    int S;
+    cin >> S;
+ 
+    int L = S/100;
+    int R = S%100;
+ 
+    if (1<=L && L<=12){
+        if(1<=R && R<=12)cout << "AMBIGUOUS" << endl;
+        else cout << "MMYY" << endl;
+    }
+    else{
+        if(1<=R && R<=12)cout << "YYMM" << endl;
+        else cout << "NA" << endl;
+    }
+ 
+}
