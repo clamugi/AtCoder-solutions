12e287eff86e75fe81e8ec2d84d96bb9ee5f9d3c
clamugi <teru_10cu@yahoo.co.jp>
2021-03-13 22:51:10 +0900

    195

---
+++ ABC/195/195c.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+#define repr(i,a,b) for (int i=a; i<b; i++)
+#define rep(i,n) for (int i=0; i<  n; i++)
+#define PI 3.14159265359  
+const long long INF = 1LL << 60;
+long long MOD = 1000000007;
+long long gcd(long long a, long long b) { return b ? gcd(b, a%b) : a; }
+long long lcm (int a, int b){return  (long long) a*b /gcd(a,b);}
+double radian(double degree){return degree * PI /180.0;}
+double degree(double radian){return radian * 180.0/PI;}
+
+vector<pair<long long, long long> > prime_factorize(long long N){
+    vector<pair< long long , long long> > res;
+    for(long long a = 2; a* a <= N; a++){
+        if(N%a != 0)continue;
+        long long ex = 0;
+        while(N%a == 0){
+            ex++;
+            N /= a;
+        }
+        res.push_back({a,ex});
+    }
+    if(N!=1)res.push_back({N,1});
+    return res;
+}
+
+int main(){
+    long long N;
+    cin >> N;
+
+    long long ans = 0;
+    long long b = 1000;
+    for(long long a = 0; a<=5; a++){
+        if(N>=b){
+            ans +=(long long)a * 999 * pow(1000,a);
+            b*= 1000;
+        }
+        else {
+            ans += a *(N- (b/1000) + 1 );
+            break;
+        }
+
+    }
+    cout << ans << endl;
+}
