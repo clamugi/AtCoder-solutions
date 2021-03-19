b0445116f5e162460d01b22bbebb47b39aac99a1
clamugi <teru_10cu@yahoo.co.jp>
2021-03-19 15:49:29 +0900

    177

---
+++ ABC/177/177d.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+#define repr(i,a,b) for (int i=a; i<b; i++)
+#define rep(i,n) for (int i=0; i<  n; i++)
+#define PI 3.14159265359  
+const long long INF = 1LL << 60;
+long long MOD = 1000000007;
+long long gcd(long long a, long long b) { return b ? gcd(b, a%b) : a; }
+long long lcm (int a, int b){return  (long long) a*b /gcd(a,b);}
+double radian(double degree){return degree * PI /180.0;}
+double degree(double radian){return radian * 180.0/PI;}
+
+struct UnionFind {
+    vector<int> par;
+    
+    UnionFind(int n) : par(n, -1) { }
+    void init(int n) { par.assign(n, -1); }
+    
+    int root(int x) {
+        if (par[x] < 0) return x;
+        else return par[x] = root(par[x]);
+    }
+    
+    bool issame(int x, int y) {
+        return root(x) == root(y);
+    }
+    
+    bool merge(int x, int y) {
+        x = root(x); y = root(y);
+        if (x == y) return false;
+        if (par[x] > par[y]) swap(x, y);
+        par[x] += par[y];
+        par[y] = x;
+        return true;
+    }
+    
+    int size(int x) {
+        return -par[root(x)];
+    }
+};
+
+
+int main(){
+    int N, M;
+    cin >> N >> M;
+
+    UnionFind uf(N);
+    for (int i = 0; i < M; ++i) {
+        int a, b; cin >> a >> b; --a, --b;
+        uf.merge(a, b);
+    }
+    int res = 0;
+    for (int i = 0; i < N; ++i) res = max(res, uf.size(i));
+    cout << res << endl;
+
+
+}
