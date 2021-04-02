03e4c9f83cb5d2077fe5804cf4c5a55727000df3
clamugi <teru_10cu@yahoo.co.jp>
2021-04-02 19:49:46 +0900

    166

---
+++ ABC/166/166a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    
+    string s;
+    cin >> s;
+ 
+    if (s == "ABC") {
+        s = "ARC";
+    }
+ 
+    else {
+        s = "ABC";
+    }
+ 
+    cout << s << endl;
+    return 0;
+ }
