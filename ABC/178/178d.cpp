003886d056df966311ba6622479f3cb0110c3167
clamugi <teru_10cu@yahoo.co.jp>
2021-04-07 12:04:19 +0900

    178

---
+++ ABC/178/178d.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+#define repr(i,a,b) for (int i=a; i<b; i++)
+#define rep(i,n) for (int i=0; i<  n; i++)
+#define PI 3.14159265359  
+const long long INF = 1LL << 60;
+long long MOD = 1000000007;
+long long gcd(long long a, long long b) { return b ? gcd(b, a%b) : a; }
+long long lcm (int a, int b){return  (long long) a*b /gcd(a,b);}
+double radian(double degree){return degree * PI /180.0;}
+double degree(double radian){return radian * 180.0/PI;}
+
+/* nCr mod */
+const int MAX = 510000;
+long long fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+// 二項係数計算
+long long COM(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+
+
+int main(){
+    long long S;
+    cin >> S;
+
+    COMinit();
+
+    long long ans = 0;
+    if(S>=3)ans += 1;
+
+    for(long long i = 2; i*3<=S; i++){
+        long long ball = S- i*3;
+        long long box = i-1;
+
+        ans += COM(ball+box,ball) %MOD;
+    }
+    cout << ans%MOD << endl;
+}
+
+
+
+
