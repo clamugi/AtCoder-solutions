7043c2beb4449cb4286847f410c964429f7be5fd
clamugi <teru_10cu@yahoo.co.jp>
2021-04-15 14:39:00 +0900

    156

--- sample.cpp
+++ sample.cpp
@@ -126,3 +126,20 @@ long long COM(int n, int k){
     if (n < 0 || k < 0) return 0;
     return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
 }
+
+
+// a^n mod を計算する
+long long modpow(long long a, long long n, long long mod) {
+    long long res = 1;
+    while (n > 0) {
+        if (n & 1) res = res * a % mod;
+        a = a * a % mod;
+        n >>= 1;
+    }
+    return res;
+}
+
+/* 
+フェルマーの小定理について
+https://qiita.com/ofutonfuton/items/92b1a6f4a7775f00b6ae 
+*/
