11a28432d207eaeb0453ad68f4fbf41d8e5bd5f9
clamugi <teru_10cu@yahoo.co.jp>
2021-05-08 22:44:45 +0900

    200

---
+++ ABC/200/200b.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+#define repr(i,a,b) for (int i=a; i<b; i++)
+#define rep(i,n) for (int i=0; i<  n; i++)
+#define PI 3.14159265359  
+const long long INF = 1LL << 60;
+long long MOD = 1000000007;
+long long gcd(long long a, long long b) { return b ? gcd(b, a%b) : a; }
+long long lcm (int a, int b){return  (long long) a*b /gcd(a,b);}
+double radian(double degree){return degree * PI /180.0;}
+double degree(double radian){return radian * 180.0/PI;}
+
+int main(){
+    long long N, K;
+    cin >> N >> K;
+
+    for(long long i = 0; i<K; i++){
+        if(N%200 == 0)N/=200;
+        else{
+            N= N*1000+200;
+        }
+    }
+    cout << N << endl;
+}
---
+++ ABC/200/200c.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+#define repr(i,a,b) for (int i=a; i<b; i++)
+#define rep(i,n) for (int i=0; i<  n; i++)
+#define PI 3.14159265359  
+const long long INF = 1LL << 60;
+long long MOD = 1000000007;
+long long gcd(long long a, long long b) { return b ? gcd(b, a%b) : a; }
+long long lcm (int a, int b){return  (long long) a*b /gcd(a,b);}
+double radian(double degree){return degree * PI /180.0;}
+double degree(double radian){return radian * 180.0/PI;}
+
+int main(){
+    int N;
+    cin >> N;
+
+    vector<int>A(N);
+    rep(i,N)cin >> A[i];
+
+    rep(i,N) A[i]%= 200;
+    sort(A.begin(),A.end());
+
+
+    long long ans = 0;
+    for(int i = 0; i<N; i++){
+        auto lo = lower_bound(A.begin(),A.end(),A[i]);
+        auto up = upper_bound(A.begin(),A.end(),A[i]);
+        ans += up - lo - 1;
+    }
+    cout << ans/2 << endl;
+}
