6ef09f661b99796c8250c0a755ebad6473828f25
clamugi <teru_10cu@yahoo.co.jp>
2021-05-13 13:11:50 +0900

    057

---
+++ ARC/057/057a.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+#define repr(i,a,b) for (int i=a; i<b; i++)
+#define rep(i,n) for (int i=0; i<  n; i++)
+#define PI 3.14159265359  
+const long long INF = 1LL << 60;
+long long MOD = 1000000007;
+long long gcd(long long a, long long b) { return b ? gcd(b, a%b) : a; }
+long long lcm (int a, int b){return  (long long) a*b /gcd(a,b);}
+double radian(double degree){return degree * PI /180.0;}
+double degree(double radian){return radian * 180.0/PI;}
+
+int main(){
+    long long A, K;
+    cin >> A >> K;
+
+    long long t = pow(10,12);
+    t *= 2;
+
+    if(K == 0){
+        cout << t-A << endl; 
+    }
+    else{
+
+        for(int i = 1; i<=100; i++){
+            if(A >= (t -(1+K*A)) ) {
+                cout << i << endl;
+                return 0;
+            } 
+            A += (1+K*A);
+        }
+    }
+
+
+}
