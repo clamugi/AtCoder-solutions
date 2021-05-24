d3423df3c4c35a41b2e5e434f37cd199ae8cef16
clamugi <teru_10cu@yahoo.co.jp>
2021-05-24 16:47:54 +0900

    202b

---
+++ ABC/201~300/202/202b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+#define repr(i,a,b) for (int i=a; i<b; i++)
+#define rep(i,n) for (int i=0; i<  n; i++)
+#define PI 3.14159265359  
+const long long INF = 1LL << 60;
+long long MOD = 1000000007;
+long long gcd(long long a, long long b) { return b ? gcd(b, a%b) : a; }
+long long lcm (int a, int b){return  (long long) a*b /gcd(a,b);}
+double radian(double degree){return degree * PI /180.0;}
+double degree(double radian){return radian * 180.0/PI;}
+
+int main(){
+    string s;
+    cin >> s;
+
+    for(int i=s.length()-1;i>=0; i-- ){
+        if(s[i] == '6')cout << '9' ;
+        else if(s[i] == '9')cout << '6';
+        else cout << s[i];
+    }
+    cout << endl;   
+}
